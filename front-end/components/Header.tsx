0651e38549feef1d61174b30c1a88b87a4a69d9d
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-11-11 12:10:59 +0300

    Remove backdrop blur from Header component

--- front-end/components/Header.tsx
+++ front-end/components/Header.tsx
@@ -33,7 +33,7 @@ export function Header() {
   }
 
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 backdrop-blur shadow-md">
+    <div className="fixed top-0 left-0 right-0 z-50 shadow-md">
       <div className="flex justify-center">
         <div className="w-full max-w-lg">
           <Card className="rounded-none border-x-0 border-t-0 shadow-none">
